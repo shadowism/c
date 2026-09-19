6f29417fac0516810e582301d534828a973efad9
shadow <shadow@hotmail.com>
2026-09-19 15:35:18 +0800

    1_1变量与赋值

---
+++ 1_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+int age;            // 定义一个整型变量，贴标签 age
+age = 18;           // 赋值：把 18 放进去
+int score = 100;    // 定义的同时赋初值（初始化）
+printf("%d\n", age);
+printf("%d\n",score);
+} // 取出 age 盒子里的值打印：18
