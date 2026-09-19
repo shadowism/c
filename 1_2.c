f1f46fa1f5a60ad00f0425e638d5470fe8fd343e
shadow <shadow@hotmail.com>
2026-09-19 15:45:40 +0800

    1_2四种数据类型及其占用字节大小

---
+++ 1_2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("int    占 %d 字节\n", sizeof(int));
+    printf("float  占 %d 字节\n", sizeof(float));
+    printf("double 占 %d 字节\n", sizeof(double));
+    printf("char   占 %d 字节\n", sizeof(char));
+    return 0;
+}
