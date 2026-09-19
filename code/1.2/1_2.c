1f4dfeabfe206fa3094b64c4ca29ec7a703050a0
shadow <shadow@hotmail.com>
2026-09-19 16:49:45 +0800

    1_6printf与占位符的初步使用

--- code/1.2/1_2.c
+++ code/1.2/1_2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    printf("int    占 %d 字节\n", sizeof(int));
+    printf("int    占 %d 字节\n", sizeof(int));//sizeof()函数可以查看变量的字节数量
     printf("float  占 %d 字节\n", sizeof(float));
     printf("double 占 %d 字节\n", sizeof(double));
     printf("char   占 %d 字节\n", sizeof(char));
---
+++ code/1.6/1_6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int age = 18;
+    float height = 1.72f;//f表示单精度浮点数
+    char grade = 'a';
+    printf("年龄%d岁，身高%.2f米，等级%c\n", age, height, grade);
+    //%d表示整数，%.2f表示浮点数，%c表示字符，%n表示换行
+    return 0;
+}
